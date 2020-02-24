18baa0255c7da7c663c0eebaedbc40a782d1c658
PavelKondrik <pavelofficial@ya.ru>
2020-02-24 20:55:27 +0300

    Routing refactoring draft. Part 2.

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,18 +1,7 @@
-import React, { Component } from 'react';
+import React, { ReactElement } from 'react';
 import AppLayout from 'components/AppLayout';
 import { Helmet } from 'react-helmet';
 import { Switch, Route } from 'react-router';
-
-import {connect} from 'app/extentions/react-redux'; // todo: Make it works.
-
-// import { getRouterLocation } from './selectors/router';
-/*
-export interface AppComponentProps {
-    // location: any,
-    route: RouteConfig
-}
-*-/
- */
 
 import Temp from './Temp';
 import NotFound from './NotFound';
@@ -20,72 +9,48 @@ import RegisterPage from './auth/RegisterPage';
 import LoginPage from './auth/LoginPage';
 import RecoverPage from './auth/RecoverPage';
 import Dashboard from './shop/Dashboard';
-// import { RouteConfig } from 'react-router-config';
 
-/*
-import Loadable from 'react-loadable';
-import Loading from 'components/Loading';
-
-const UsersLoadable = Loadable({
-  loader: () => import('./../../pages/Users'),
-  loading: Loading,
-});
-*/
-
-@(connect(() => ({
-    // location: getRouterLocation(state), todo: Make it.
-}), null) as any)
-class App extends Component<AppComponentProps, {}> {
-/*
-    static propTypes = {
-        location: PropTypes.shape({}).isRequired,
-        route: PropTypes.shape({}).isRequired,
-    };
-*/
-
-    renderSiteMeta() {
-        // const canonical = this.props.location.toJS().pathname.toLowerCase();
-        return (<Helmet
-                link={[/*{
+const SiteMeta = ():ReactElement => {
+    return (<Helmet
+            link={[/*{
                     href: canonical,
                     rel: 'canonical',
                 }*/]}
-                meta={[{
-                    name: 'viewport',
-                    content: 'width=device-width, initial-scale=1',
-                }]}
-            />
-        );
-    }
+            meta={[{
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            }]}
+        />
+    );
+};
 
-    render() {
+const App = ():ReactElement => {
 
-        return (
-            <AppLayout>
-                {this.renderSiteMeta()}
-                <Switch>
-                    <Route exact path="/">
-                        <Temp />
-                    </Route>
-                    <Route path="/register">
-                        <RegisterPage />
-                    </Route>
-                    <Route path="/login">
-                        <LoginPage />
-                    </Route>
-                    <Route path="/recover">
-                        <RecoverPage />
-                    </Route>
-                    <Route path="/dashboard">
-                        <Dashboard />
-                    </Route>
-                    <Route path="*">
-                        <NotFound />
-                    </Route>
-                </Switch>
-            </AppLayout>
-        );
-    }
-}
+    return (
+        <AppLayout>
+            <SiteMeta />
+            <Switch>
+                <Route exact path="/">
+                    <Temp />
+                </Route>
+                <Route path="/register">
+                    <RegisterPage />
+                </Route>
+                <Route path="/login">
+                    <LoginPage />
+                </Route>
+                <Route path="/recover">
+                    <RecoverPage />
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard />
+                </Route>
+                <Route path="*">
+                    <NotFound />
+                </Route>
+            </Switch>
+        </AppLayout>
+    );
+};
 
 export default App;
