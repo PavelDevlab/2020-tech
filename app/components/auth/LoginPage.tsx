9cec0b6f1f87a964f4d20df3d36bef38bab538df
PavelDevlab <pavel-devlab@ya.ru>
2020-02-27 14:47:49 +0300

    Form styles.

--- app/components/auth/LoginPage.tsx
+++ app/components/auth/LoginPage.tsx
@@ -11,6 +11,12 @@ import { FormikValues, FormikHelpers } from 'formik/dist/types';
 import { LoginValues } from './types/LoginPage';
 import {connect} from 'react-redux';
 
+
+import classNames from 'classnames';
+
+import withStyles from 'isomorphic-style-loader/withStyles';
+import s from 'app/components/common/form.scss';
+
 const initialValues:LoginValues = {
     login: '',
     password: '',
@@ -43,35 +49,43 @@ const LoginPage:FunctionComponent<LoginPageProps> = ({ onSubmit }: LoginPageProp
         onSubmit={ onSubmit }
       >
           {(props:FormikProps<LoginValues>) => (
-            <Form>
-                <h2>
-                  Sign in
-                </h2>
-                <div>
-                    <label>login</label>
-                    <Field
-                      type="text"
-                      name="login"
-                    />
-                </div>
-                <ErrorMessage name="login" />
-                <div>
-                    <label>password</label>
-                    <Field
-                      type="password"
-                      name="password"
-                    />
-                </div>
-                <ErrorMessage name="password" />
-                <br />
-                <button type="submit"
-                        disabled={props.isSubmitting}>
-                    Submit
-                </button>
-                <br />
-                <br />
+            <div>
+              <Form className={classNames(s['form'], s['_compact'])}>
+                  <h3>
+                    Sign in
+                  </h3>
+                  <div className={classNames(s['field-group'])}>
+                      <label className={s['label']}>login</label>
+                      <Field
+                        className={s['input']}
+                        type="text"
+                        name="login"
+                      />
+                      <div className={classNames(s['error'])}>
+                          <ErrorMessage name="login" />
+                      </div>
+                  </div>
+                  <div className={classNames(s['field-group'])}>
+                      <label className={s['label']}>password</label>
+                      <Field
+                        className={s['input']}
+                        type="password"
+                        name="password"
+                      />
+                      <div className={classNames(s['error'])}>
+                          <ErrorMessage name="password" />
+                      </div>
+                  </div>
+                  <button type="submit"
+                          className={s['btn']}
+                          disabled={props.isSubmitting}>
+                      Sign in
+                  </button>
+              </Form>
+              <div className={classNames(s['error'], s['_big'], s['form__result-error'])}>
                 <ErrorMessage name="form" />
-            </Form>
+              </div>
+            </div>
           )}
       </Formik>
     );
@@ -90,5 +104,6 @@ export default compose(
             }
         };
     }
-  )(LoginPage as any)
-);
+  ),
+  withStyles(s)
+)(LoginPage as any);
--- app/components/auth/RegisterPage.tsx
+++ app/components/auth/RegisterPage.tsx
@@ -7,6 +7,11 @@ import { compose } from 'redux';
 import PropTypes, { InferProps } from "prop-types";
 import { registerPerson } from 'app/redux/ducks/auth';
 
+import classNames from 'classnames';
+
+import withStyles from 'isomorphic-style-loader/withStyles';
+import s from 'app/components/common/form.scss';
+
 import { FormikValues, FormikHelpers } from 'formik/dist/types';
 import { RegisterValues } from './types/RegisterPage';
 import {connect} from 'react-redux';
@@ -53,44 +58,55 @@ const RegisterPage:FunctionComponent<RegisterPageProps> = ({ onSubmit }: Registe
             onSubmit={ onSubmit }
         >
             {(props:FormikProps<RegisterValues>) => (
-                <Form>
-                    <h2>
+              <div>
+                <Form className={classNames(s['form'], s['_compact'])}>
+                    <h3>
                       Sign up
-                    </h2>
-                    <div>
-                        <label>login</label>
+                    </h3>
+                    <div className={classNames(s['field-group'])}>
+                        <label className={s['label']}>login</label>
                         <Field
+                            className={s['input']}
                             type="text"
                             name="login"
                         />
+                      <div className={classNames(s['error'])}>
+                        <ErrorMessage name="login" />
+                      </div>
                     </div>
-                    <ErrorMessage name="login" />
-                    <div>
-                        <label>password</label>
+                    <div className={classNames(s['field-group'])}>
+                        <label className={s['label']}>password</label>
                         <Field
+                            className={s['input']}
                             type="password"
                             name="password"
                         />
+                      <div className={classNames(s['error'])}>
+                        <ErrorMessage name="password" />
+                      </div>
                     </div>
-                    <ErrorMessage name="password" />
 
-                    <div>
-                      <label>password repeat</label>
+                    <div className={classNames(s['field-group'])}>
+                      <label className={s['label']}>password repeat</label>
                       <Field
+                        className={s['input']}
                         type="password"
                         name="passwordRepeat"
                       />
+                      <div className={classNames(s['error'])}>
+                        <ErrorMessage name="passwordRepeat" />
+                      </div>
                     </div>
-                    <ErrorMessage name="passwordRepeat" />
-                    <br />
                     <button type="submit"
+                            className={s['btn']}
                             disabled={props.isSubmitting}>
-                        Submit
+                        Sign up
                     </button>
-                    <br />
-                    <br />
-                    <ErrorMessage name="form" />
                 </Form>
+                <div className={classNames(s['error'], s['_big'], s['form__result-error'])}>
+                  <ErrorMessage name="form" />
+                </div>
+              </div>
             )}
         </Formik>
     );
@@ -109,5 +125,6 @@ export default compose(
                 }
             };
         }
-    )(RegisterPage as any)
-);
+    ),
+    withStyles(s)
+)(RegisterPage as any);
