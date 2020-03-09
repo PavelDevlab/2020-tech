11d9e96bd6da4f56e99836571eb816e12ce3a9dd
PavelDevlab <pavel-devlab@ya.ru>
2020-03-09 14:49:43 +0300

    Prop types refactoring update for App.

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,23 +1,18 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import AppLayout from 'app/components/AppLayout/index';
 import { Helmet } from 'react-helmet';
 import { Switch, Route } from 'react-router';
-// import { signedInSelector } from 'app/redux/ducks/auth';
-// import Immutable from 'immutable';
 
 import MainPage from './MainPage';
 import NotFound from './NotFound';
 import RegisterPage from './auth/RegisterPage';
 import LoginPage from './auth/LoginPage';
-// import RecoverPage from './auth/RecoverPage';
+
 import Dashboard from './shop/Dashboard';
 import { PrivateRoute, PublicRoute } from 'components/AuthRoute';
 
-// import { connect } from 'react-redux';
-// import { compose } from 'redux';
-import PropTypes, { /* InferProps */ } from 'prop-types';
 
-const SiteMeta = ():ReactElement => {
+const SiteMeta:React.FunctionComponent<{}> = () => {
     return (<Helmet
             link={[/*{
                     href: canonical,
@@ -31,17 +26,7 @@ const SiteMeta = ():ReactElement => {
     );
 };
 
-const propTypes = {
-  signedIn: PropTypes.bool.isRequired
-};
-
-const defaultProps = {
-  signedIn: false
-};
-
-// type AppProps = InferProps<typeof propTypes>;
-
-const App = ():ReactElement => {
+const App:React.FunctionComponent<{}> = () => {
 
     return (
         <AppLayout>
@@ -67,17 +52,4 @@ const App = ():ReactElement => {
     );
 };
 
-App.defaultProps = defaultProps;
-App.propTypes = propTypes;
-
-
 export default App;
-/*
-export default compose(
-  connect((state: Immutable.Map<string, any>) => {
-    return {
-      signedIn: signedInSelector(state),
-    };
-  })
-)(App as any);
- */
